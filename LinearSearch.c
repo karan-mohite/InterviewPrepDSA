61d49086ddf47989b0e8a4382ea47aa830b54242
karan-mohite <142478056+karan-mohite@users.noreply.github.com>
2025-10-06 12:00:49 +0530

    .

--- LinearSearch.c
+++ LinearSearch.c
@@ -1,9 +1,15 @@
+//WAP to search a number using linear search and create a user defined function.
+
 void main()
 {
-    int i,a[100],search;
-    int k=0;
-    printf("Enter 10 numbers:");
-    for(i=0;i<10;i++)
+    int i,a[10],search;
+    int k=0,r;
+
+    printf("Enter a range:");
+    scanf("%d",&r);
+
+    printf("Enter %d numbers:",r);
+    for(i=0;i<r;i++)
     {
         scanf("%d",&a[i]);
     }
@@ -11,11 +17,11 @@ void main()
     printf("Enter a number that you want to search:");
     scanf("%d",&search);
 
-    for(i=0;i<10;i++)
+    for(i=0;i<r;i++)
     {
         if(a[i]==search)
         {
-            printf("Number is found..!!");
+            printf("Number is found at position %d",i+1);
             k++;
             break;
         }
