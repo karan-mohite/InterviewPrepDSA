78ef06e34289fd6eb4ed96fa8aa4c8d3c80252cf
karan-mohite <142478056+karan-mohite@users.noreply.github.com>
2025-10-06 11:46:52 +0530

    .

--- LinearSearch.c
+++ LinearSearch.c
@@ -1,10 +1,28 @@
 void main()
 {
-    int i,a[100];
+    int i,a[100],search;
+    int k=0;
     printf("Enter 10 numbers:");
     for(i=0;i<10;i++)
     {
         scanf("%d",&a[i]);
+    }
+
+    printf("Enter a number that you want to search:");
+    scanf("%d",&search);
+
+    for(i=0;i<10;i++)
+    {
+        if(a[i]==search)
+        {
+            printf("Number is found..!!");
+            k++;
+            break;
+        }
+    }
+    if(k==0)
+    {
+        printf("Number is not found..!!");
     }
     getch();
 }
