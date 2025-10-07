3ee98702f68078ca3aa9e6481658915842a21ca1
karan-mohite <142478056+karan-mohite@users.noreply.github.com>
2025-10-07 09:37:15 +0530

    .

--- LinkedList.c
+++ LinkedList.c
@@ -5,11 +5,10 @@ struct node
 };
 
 struct node *start=0;       //Global declaration
-
 struct node *createNode()
 {
     struct node *h1;
-    h1=(struct node *)malloc(sizeOf(struct node));
+    h1=(struct node *)malloc(sizeof(struct node));
     printf("Enter a data:");
     scanf("%d",&h1->data);
     h1->next=0;
@@ -17,9 +16,17 @@ struct node *createNode()
 }
 void insertStart()
 {
-   struct node *k1;
+  struct node *k1;
   k1=createNode();
-  if(start=0)
+  if(start==0)
+  {
+      start=k1;
+  }
+  else
+  {
+      k1->next=start;
+      start=k1;
+  }
 }
 void insertEnd()
 {
@@ -43,13 +50,19 @@ void deleteMiddle()
 }
 void display()
 {
+      struct node *j1;
+      j1=start;
 
+      while(j1!=0)
+      {
+          printf("%d ",j1->data);
+          j1=j1->next;
+      }
 }
 
 void main()
 {
-    while(1)
-    {
+
         int ch;
 
         printf("\n1. Insertion from start");
@@ -61,6 +74,8 @@ void main()
         printf("\n7. Display");
         printf("\n8. Exit");
 
+    while(1)
+    {
         printf("\n\nEnter Your Choice:");
         scanf("%d", &ch);
 
@@ -96,7 +111,6 @@ void main()
 
             case 8:
                 exit(0);
-
             default:
                 printf("Wrong Input");
         }
